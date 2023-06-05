8e18e1c2bc78c881470fb868af5af3823e8990d7
athiruj.k <athirujkaewseesuk@gmail.com>
2023-06-05 08:05:20 +0700

    edit generate_data

--- generate_data.py
+++ generate_data.py
@@ -1,5 +1,4 @@
-
-# manage & generate data 
+# manage & generate data
 
 import sys
 import os
@@ -11,8 +10,11 @@ from logging_con import logger
 
 from tqdm import tqdm
 
-
+########################################################################
 # wav file input
+########################################################################
+
+
 def file_load(wav_name, mono=False):
     """
     load .wav file.
@@ -30,6 +32,11 @@ def file_load(wav_name, mono=False):
         return librosa.load(wav_name, sr=None, mono=mono)
     except:
         logger.error("file_broken or not exists!! : {}".format(wav_name))
+
+
+########################################################################
+
+########################################################################
 
 
 def demux_wav(wav_name, channel=0):
@@ -56,17 +63,15 @@ def demux_wav(wav_name, channel=0):
         return sr, numpy.array(multi_channel_data)[channel, :]
 
     except ValueError as msg:
-        logger.warning(f'{msg}')
+        logger.warning(f"{msg}")
+
 
 ########################################################################
 # feature extractor
 ########################################################################
-def file_to_vector_array(file_name,
-                         n_mels=64,
-                         frames=5,
-                         n_fft=1024,
-                         hop_length=512,
-                         power=2.0):
+def file_to_vector_array(
+    file_name, n_mels=64, frames=5, n_fft=1024, hop_length=512, power=2.0
+):
     """
     convert file_name to a vector array.
 
@@ -77,97 +82,124 @@ def file_to_vector_array(file_name,
         vector array
         * dataset.shape = (dataset_size, fearture_vector_length)
     """
-    # 01 calculate the number of dimensions
+    # calculate the number of dimensions
+    # คำนวณมิติของชุดข้อมูล
     dims = n_mels * frames
 
-    # 02 generate melspectrogram using librosa (**kwargs == param["librosa"])
+    # generate melspectrogram using librosa (**kwargs == param["librosa"])
     sr, y = demux_wav(file_name)
-    mel_spectrogram = librosa.feature.melspectrogram(y=y,
-                                                     sr=sr,
-                                                     n_fft=n_fft,
-                                                     hop_length=hop_length,
-                                                     n_mels=n_mels,
-                                                     power=power)
+    mel_spectrogram = librosa.feature.melspectrogram(
+        y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels, power=power
+    )
 
-    # 03 convert melspectrogram to log mel energy
-    log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
+    # convert melspectrogram to log mel energy
+    log_mel_spectrogram = (
+        20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
+    )
 
-    # 04 calculate total vector size
-    vectorarray_size = len(log_mel_spectrogram[0, :]) - frames + 1
+    # calculate total vector size
+    vector_array_size = len(log_mel_spectrogram[0, :]) - frames + 1
 
     # 05 skip too short clips
-    if vectorarray_size < 1:
+    if vector_array_size < 1:
         return numpy.empty((0, dims), float)
 
     # 06 generate feature vectors by concatenating multi_frames
-    vectorarray = numpy.zeros((vectorarray_size, dims), float)
+    vector_array = numpy.zeros((vector_array_size, dims), float)
+
     for t in range(frames):
-        vectorarray[:, n_mels * t: n_mels * (t + 1)] = log_mel_spectrogram[:, t: t + vectorarray_size].T
+        vector_array[:, n_mels * t : n_mels * (t + 1)] = log_mel_spectrogram[
+            :, t : t + vector_array_size
+        ].T
 
-    return vectorarray
+    return vector_array
 
-def list_to_vector_array(file_list,
-                         msg="calc...",
-                         n_mels=64,
-                         frames=5,
-                         n_fft=1024,
-                         hop_length=512,
-                         power=2.0):
-  
+
+def list_to_vector_array(
+    file_list, msg="calc...", n_mels=64, frames=5, n_fft=1024, hop_length=512, power=2.0
+):
     # 01 calculate the number of dimensions
     dims = n_mels * frames
 
     # 02 loop of file_to_vectorarray
     for idx in tqdm(range(len(file_list)), desc=msg):
-
-        vector_array = file_to_vector_array(file_list[idx],
-                                            n_mels=n_mels,
-                                            frames=frames,
-                                            n_fft=n_fft,
-                                            hop_length=hop_length,
-                                            power=power)
+        vector_array = file_to_vector_array(
+            file_list[idx],
+            n_mels=n_mels,
+            frames=frames,
+            n_fft=n_fft,
+            hop_length=hop_length,
+            power=power,
+        )
 
         if idx == 0:
             dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
 
-        dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
+        dataset[
+            vector_array.shape[0] * idx : vector_array.shape[0] * (idx + 1), :
+        ] = vector_array
 
     return dataset
 
-# dataset generator #
 
-def dataset_generator(target_dir,
-                      normal_dir_name="normal",
-                      abnormal_dir_name="abnormal",
-                      ext="wav"):
-  
+########################################################################
+# dataset generator #
+########################################################################
+
+
+def dataset_generator(
+    target_dir, normal_dir_name="normal", abnormal_dir_name="abnormal", ext="wav"
+):
+    """
+    generater dataset
+    
+    taeget_drr: str
+    """
     logger.info("target_dir : {}".format(target_dir))
 
-      # 01 normal list generate
-    normal_files = sorted(glob.glob(
-        os.path.abspath("{dir}/{normal_dir_name}/*.{ext}".format(dir=target_dir, normal_dir_name=normal_dir_name, ext=ext))))
+    # 01 normal list generate
+    normal_files = sorted(
+        glob.glob(
+            os.path.abspath(
+                "{dir}/{normal_dir_name}/*.{ext}".format(
+                    dir=target_dir, normal_dir_name=normal_dir_name, ext=ext
+                )
+            )
+        )
+    )
     print(normal_files)
-    
+
     normal_labels = numpy.zeros(len(normal_files))
     if len(normal_files) == 0:
         logger.exception("no_wav_data!!")
 
-      # 02 abnormal list generate
-    abnormal_files = sorted(glob.glob(
-        os.path.abspath("{dir}/{abnormal_dir_name}/*.{ext}".format(dir=target_dir, abnormal_dir_name=abnormal_dir_name, ext=ext))))
+    # 02 abnormal list generate
+    abnormal_files = sorted(
+        glob.glob(
+            os.path.abspath(
+                "{dir}/{abnormal_dir_name}/*.{ext}".format(
+                    dir=target_dir, abnormal_dir_name=abnormal_dir_name, ext=ext
+                )
+            )
+        )
+    )
     print(abnormal_files)
-    
+
     abnormal_labels = numpy.ones(len(abnormal_files))
     if len(abnormal_files) == 0:
         logger.exception("no_wav_data!!")
 
-      # 03 separate train & eval
-    train_files = normal_files[len(abnormal_files):]
-    train_labels = normal_labels[len(abnormal_files):]
-    
-    eval_files = numpy.concatenate((normal_files[:len(abnormal_files)], abnormal_files), axis=0)
-    eval_labels = numpy.concatenate((normal_labels[:len(abnormal_files)], abnormal_labels), axis=0)
-    
+    # 03 separate train & eval
+    train_files = normal_files[len(abnormal_files) :]
+    train_labels = normal_labels[len(abnormal_files) :]
+
+    eval_files = numpy.concatenate(
+        (normal_files[: len(abnormal_files)], abnormal_files), axis=0
+    )
+    eval_labels = numpy.concatenate(
+        (normal_labels[: len(abnormal_files)], abnormal_labels), axis=0
+    )
+
     logger.info("train_file num : {num}".format(num=len(train_files)))
     logger.info("eval_file  num : {num}".format(num=len(eval_files)))
 
