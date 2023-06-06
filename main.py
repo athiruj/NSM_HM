b03a97ba6abcf66bab53d23043920fffcd46e44a
athiruj.k <athirujkaewseesuk@gmail.com>
2023-06-07 01:20:35 +0700

    add test func

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from visualizer import visualizer
+import librosa
+from generate_data import demux_wav
 
-print("hello")
+file = 'D:/Workfile/Project/NSM_ML/NSM_HM/dataset/fan/id_00/normal/00000000.wav'
+
+def test(file):
+    sr, y = demux_wav(file)
+    mel_spectrogram = librosa.feature.melspectrogram(
+        y=y,
+        S=None,
+        sr=sr,
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64,
+        power=2.0,
+    )
+
+    visualizer.spec_power_plot(S=mel_spectrogram,sr=sr)
+
+test(file)
